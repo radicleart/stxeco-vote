eddce1f9f5100872b9189512af6dd364af7d60cd
Mike Cohen <mjoecohen@gmail.com>
2024-01-25 10:37:50 +0000

    tidy file

--- src/routes/dao/voting/badge/[slug]/+page.svelte
+++ src/routes/dao/voting/badge/[slug]/+page.svelte
@@ -7,7 +7,7 @@ import { loggedIn } from "$lib/stacks_connect";
 import { goto } from "$app/navigation";
 import type { ProposalEvent } from "$types/stxeco.type";
 import ProposalHeader from "$lib/components/all-voters/ProposalHeader.svelte";
-	import BadgeClaim from "$lib/components/all-voters/solo/BadgeClaim.svelte";
+	import BadgeClaim from "$lib/components/all-voters/BadgeClaim.svelte";
 
 let proposal:ProposalEvent;
 
